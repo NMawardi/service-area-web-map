cafb21304d7a93d09f7df1451751fd60826a9b03
NMawardi <nizam.mawardi@bmkg.go.id>
2025-06-01 07:17:01 +0700

    Add files via upload

---
+++ styles/Joinedlayer_4_style.js
@@ -0,0 +1,65 @@
+var size = 0;
+var placement = 'point';
+function categories_Joinedlayer_4(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case '500':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(253,231,37,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '1000':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(53,183,121,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '1500':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(49,104,142,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(237,14,21,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_Joinedlayer_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("qneat_cost_level");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_Joinedlayer_4(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
